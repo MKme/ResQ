f6bab84e9a647a01438fe42bfbbc25b01775145c
Eric <MKme@users.noreply.github.com>
2020-09-22 20:39:33 -0400

    ResQ Camera Updates
    Reverted whitebalance settings to Auto in main code

--- Code/Camera Module ESP32 Code/ResQ_Camera_Module_V1.2/ResQ_Camera_Module_V1.2.ino
+++ Code/Camera Module ESP32 Code/ResQ_Camera_Module_V1.2/ResQ_Camera_Module_V1.2.ino
@@ -109,7 +109,7 @@ void setup() {
   s->set_special_effect(s, 0); // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
   s->set_whitebal(s, 1);       // 0 = disable , 1 = enable
   s->set_awb_gain(s, 1);       // 0 = disable , 1 = enable
-  s->set_wb_mode(s, 3);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+  s->set_wb_mode(s, 0);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
   s->set_exposure_ctrl(s, 1);  // 0 = disable , 1 = enable
   s->set_aec2(s, 0);           // 0 = disable , 1 = enable
   s->set_ae_level(s, 0);       // -2 to 2
